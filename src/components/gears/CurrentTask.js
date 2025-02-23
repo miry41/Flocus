6ed8f17f0847b184eacc0e2fc2e6c8b5e612509b
takami <takami.ku.518@gmail.com>
2025-02-23 21:37:07 +0900

    cur

--- src/components/gears/CurrentTask.js
+++ src/components/gears/CurrentTask.js
@@ -1,12 +1,36 @@
 // src/components/gears/CurrentTask.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { DragDropContext, Droppable } from '@hello-pangea/dnd';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CurrentOn from './CurrentOn';
 import EmptyTask from './EmptyTask';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 function CurrentTask() {
   const [currentTask, setCurrentTask] = useState([]);
+  const [hasTask, setHasTask] = useState(false);
+  const [data, setData] = useState({ currentTaskId: "" });
+
+  useEffect(() => {
+    const fetchCurrentTask = async () => {
+      const auth = getAuth();
+      const user = auth.currentUser;
+      if (user) {
+        const db = getFirestore();
+        const docRef = doc(db, 'users', user.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          console.log("currentTaskId:", data.currentTaskId); // ここで currentTaskId を出力
+          setData(data);
+          setHasTask(data.currentTaskId !== "");
+        }
+      }
+    };
+
+    fetchCurrentTask();
+  }, []);
 
   const onDragEnd = (result) => {
     if (!result.destination) return;
@@ -34,12 +58,12 @@ function CurrentTask() {
                         {...provided.droppableProps}
                         ref={provided.innerRef}
                       >
-                        {currentTask.length > 0 ? (
-                          // タスクがある場合は CurrentOn を表示
-                          <CurrentOn tasks={currentTask} />
-                        ) : (
+                        {data.currentTaskId === "" ? (
                           // タスクがない場合は EmptyTask を表示
                           <EmptyTask />
+                        ) : (
+                          // タスクがある場合は CurrentOn を表示
+                          <CurrentOn tasks={currentTask} />
                         )}
                         {provided.placeholder}
                       </div>
