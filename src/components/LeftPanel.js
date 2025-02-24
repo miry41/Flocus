4861d71d7fe881b76cca2e749536e68a65fee4f8
{ito11} <{ito110xyz@gmail.com}>
2025-02-24 13:51:43 +0900

    微調整

--- src/components/LeftPanel.js
+++ src/components/LeftPanel.js
@@ -1,24 +1,26 @@
-import React, { useState } from 'react'
-import AddTask from './gears/AddTask'
-import TaskLists from './gears/TaskLists'
+import React, { useState } from 'react';
+import AddTask from './gears/AddTask';
+import TaskLists from './gears/TaskLists';
 
 function LeftPanel() {
-  const [showAddTask, setShowAddTask] = useState(false)
+  const [showAddTask, setShowAddTask] = useState(false);
 
   return (
     <div className="m-3" style={{ margin: '2rem' }}>
       <div className="card h-100">
-        <div className="card-header">タスク</div>
-        <div className="card-body">
-          <button className="btn btn-primary mb-2" onClick={() => setShowAddTask(true)}>
+        <div className="card-header d-flex justify-content-between align-items-center">
+          <span>タスク</span>
+          <button className="btn btn-primary btn-sm mb-2 ms-auto" onClick={() => setShowAddTask(true)}>
             Add Task!
           </button>
+        </div>
+        <div className="card-body">
           {showAddTask && <AddTask onClose={() => setShowAddTask(false)} />}
           <TaskLists />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default LeftPanel
+export default LeftPanel;
