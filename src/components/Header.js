b96db7927afb3b91cd693816beba6468fca5cff1
{Hiroto} <{hirototorohiishiishii@gmail.com}>
2025-02-24 14:21:38 +0900

    ヘッダ調整

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,15 @@ import MyAccount from './gears/MyAccount'
 
 function Header() {
   return (
-    <div className="headerPanel d-flex justify-content-between align-items-center">
-      {/* 左端 */}
+    <div className="headerPanel d-flex justify-content-between align-items-center px-3 py-2 bg-light">
+      {/* 左側：アカウント名とログアウトボタンを横並びに配置 */}
+      <div className="d-flex align-items-center">
+        <MyAccount />
+        <SignOut className="ms-3" />
+      </div>
+      {/* 右側：UserSearch */}
       <div>
         <UserSearch />
-      </div>
-      {/* 右端 */}
-      <div className="d-flex">
-        <MyAccount />
-        <SignOut />
       </div>
     </div>
   )
