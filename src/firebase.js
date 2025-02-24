11ee50faf8982c4d77a6bbdfdca44c2df3886efa
takami <takami.ku.518@gmail.com>
2025-02-24 16:51:37 +0900

    コメント

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAmHB3XbsQpri4CxCEkvCX8-Sy9s9Xs5ms",
-  authDomain: "flocus-fd9c0.firebaseapp.com",
-  projectId: "flocus-fd9c0",
-  storageBucket: "flocus-fd9c0.firebasestorage.app",
-  messagingSenderId: "949732130467",
-  appId: "1:949732130467:web:fe734868d5e61b76204916",
-  measurementId: "G-BZL3KP1HD7"
+  apiKey: "AIzaSyAPgP75goA9jQ_kyP0ohs_7PCg6VIbK7Do",
+  authDomain: "flocus-85173.firebaseapp.com",
+  projectId: "flocus-85173",
+  storageBucket: "flocus-85173.firebasestorage.app",
+  messagingSenderId: "589779918765",
+  appId: "1:589779918765:web:8c8f4d3b5382aee6884b24",
+  measurementId: "G-MLJ2DXWPWR"
 });
 
 const db = firebaseApp.firestore();
