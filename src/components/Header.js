c94db5d609931907def7280fc0c5ede44f7bb524
{Hiroto} <{hirototorohiishiishii@gmail.com}>
2025-02-24 13:15:39 +0900

    ヘッダ調整

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import SignOut from './gears/SiginOut';
-import UserSearch from './gears/UserSearch';
-import MyAccount from './gears/MyAccount';
+import SignOut from './gears/SiginOut'
+import UserSearch from './gears/UserSearch'
+import MyAccount from './gears/MyAccount'
 
 function Header() {
   return (
-    <div className="headerPanel">
-      <UserSearch />
-      <MyAccount />
-      <SignOut />
+    <div className="headerPanel d-flex justify-content-between align-items-center">
+      {/* 左端 */}
+      <div>
+        <UserSearch />
+      </div>
+      {/* 右端 */}
+      <div className="d-flex">
+        <MyAccount />
+        <SignOut />
+      </div>
     </div>
   )
 }
