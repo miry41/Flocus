ab746dc1b2bc085a22277923378f551d68b7ecd2
{ito11} <{ito110xyz@gmail.com}>
2025-02-24 12:38:54 +0900

    白ろー

--- src/components/gears/SiginOut.js
+++ src/components/gears/SiginOut.js
@@ -6,7 +6,7 @@ import { auth } from '../../firebase';
 function SiginOut() {
   return (
     <div>
-        <Button variant="danger" onClick={() => auth.signOut()}>サインアウト</Button>
+        <Button variant="danger" size="sm"　onClick={() => auth.signOut()}>サインアウト</Button>
     </div>
   )
 }
